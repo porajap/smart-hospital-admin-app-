52319a45dac52adf90586207dd18d814e8180447
JPJAP <51898962+porajap@users.noreply.github.com>
2023-01-08 21:09:04 +0700

    url

--- lib/src/services/urls.dart
+++ lib/src/services/urls.dart
@@ -2,7 +2,7 @@ class AppUrl{
   static final String devUrl = "http://192.168.1.111:4002/app/api/dashboard";
   static final String productionUrl = "https://re-loader.com/api_smarthospital/app/api/dashboard";
 
-  static final String baseUrl = devUrl;
+  static final String baseUrl = productionUrl;
 
   static final String login = "$baseUrl/login";
 
